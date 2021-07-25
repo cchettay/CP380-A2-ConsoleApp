2942d9d75caafb046053fcadb27f7f2c2ee097c6
cchettay1995 <chinchujoseph02@gmail.com>
2021-07-25 11:44:53 -0400

    Completed Part B of Assignment 1

--- BreadmakerReport/Program.cs
+++ BreadmakerReport/Program.cs
@@ -15,9 +15,20 @@ namespace BreadmakerReport
         {
             Console.WriteLine("Welcome to Bread World");
             var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
-            var BMList = BreadmakerDb.Breadmakers
-                // TODO: add LINQ logic ...
-                //       ...
+            //var BMList = BreadmakerDb.Breadmakers
+
+            // .ToList();
+
+            var BMList = BreadmakerDb.Breadmakers.Include(rv => rv.Reviews)
+                .AsEnumerable()
+                .Select(bm => new {
+                
+                    Reviews = bm.Reviews.Count,
+                    Average = Math.Round(bm.Reviews.Average(rvobj => rvobj.stars),2),
+                    Adjust = Math.Round(ratingAdjustmentService.Adjust(bm.Reviews.Average(rvobj => rvobj.stars), bm.Reviews.Count()),2),
+                    bm.title
+                })
+                .OrderByDescending(bm =>bm.Adjust)
                 .ToList();
 
             Console.WriteLine("[#]  Reviews Average  Adjust    Description");
@@ -26,7 +37,11 @@ namespace BreadmakerReport
                 var i = BMList[j];
                 // TODO: add output
                 // Console.WriteLine( ... );
+                Console.WriteLine("\n[{0}] {1} {2} {3} {4}", j+1,i.Reviews, i.Average, i.Adjust, i.title);
+
             }
         }
     }
 }
+
+
